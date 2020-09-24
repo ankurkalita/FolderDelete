e1908ce2c9376faeb1f8f14939696c7f2cce7a9d
Kalita <ankur.kalita@hp.com>
2020-09-24 11:43:22 +0530

    First Commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FolderDelete
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Folder to delete from" + args[0]);
+            deleteEmptyDirectories(args[0]);
+        }
+
+        private static void deleteEmptyDirectories(string startLocation)
+        {
+            foreach (var directory in Directory.GetDirectories(startLocation))
+            {
+                deleteEmptyDirectories(directory);
+                if (Directory.GetFiles(directory).Length == 0 &&
+                    Directory.GetDirectories(directory).Length == 0)
+                {
+                    Directory.Delete(directory, false);
+                }
+            }
+        }
+    }
+}
